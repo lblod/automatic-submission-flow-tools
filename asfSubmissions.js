be85ae622ffd40af456ff9271b28bd6d4cbb6c8d
benjay10 <ben.behaeghe@redpencil.io>
2022-09-21 02:42:17 +0200

    Created module for Submission with info function

---
+++ asfSubmissions.js
@@ -0,0 +1,40 @@
+/**
+ * @module asfSubmissions
+ * @description Retreive and set information about Submissions in the triplestore.
+ */
+
+import * as mu from 'mu';
+import * as mas from '@lblod/mu-auth-sudo';
+import * as cts from './constants.js';
+import * as N3 from 'n3';
+import * as sjp from 'sparqljson-parse';
+const { namedNode } = N3.DataFactory;
+
+/**
+ * Get some information about the Submission, its document and its downloaded file.
+ *
+ * @public
+ * @async
+ * @function
+ * @param {namedNode} remoteDataObject - Represents the IRI of the remote data object that is linked to a submission.
+ * @returns {array(object)} An array of objects with the structure: `{ submission: namedNode, documentUrl: literal, file: namedNode, submittedDocument: namedNode }` that contains information about the Submission.
+ */
+export async function getSubmissionInfoFromRemoteDataObject(remoteDataObject) {
+  const remoteDataObjectSparql = mu.sparqlEscapeUri(remoteDataObject.value);
+  const infoQuery = `
+    ${cts.SPARQL_PREFIXES}
+    SELECT ?submission ?documentUrl ?file ?submittedDocument WHERE {
+      GRAPH ?g {
+        ?file
+          nie:dataSource ${remoteDataObjectSparql} .
+        ?submission
+          nie:hasPart ${remoteDataObjectSparql} ;
+          prov:atLocation ?documentUrl ;
+          dct:subject ?submittedDocument .
+      }
+    }`;
+
+  const response = await mas.querySudo(infoQuery);
+  const sparqlJsonParser = new sjp.SparqlJsonParser();
+  return sparqlJsonParser.parseJsonResults(response);
+}
