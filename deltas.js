b1cbe7066a05fc13ab3c93d400e406aed33497b4
benjay10 <ben.behaeghe@redpencil.io>
2022-09-21 05:01:43 +0200

    Filter deltas using predicate functions
    This is by far a clean solution, but it works. Any bit of abstraction
    can help.

--- deltas.js
+++ deltas.js
@@ -27,3 +27,32 @@ export async function getSubjects(deltas, predicate, object) {
     .map((insert) => namedNode(insert.subject.value));
   return filtered;
 }
+
+/**
+ * Get subjects after filtering the delta messages with the help of subject, predicate and object 'predicate' functions.
+ *
+ * @public
+ * @async
+ * @function
+ * @param {array(object)} deltas - The delta messages used to filter through. The datastructure is the typical delta message format with inserts, deletes, and arrays with objects with s-p-o properties.
+ * @param {namedNode} subjectPredicate - Include the deltas where the predicate returns a truthy value for this function when passed the subject of a triple.
+ * @param {namedNode} predicatePredicate - Include the deltas where the predicate returns a truthy value for this function when passed the predicate of a triple.
+ * @param {namedNode} objectPredicate - Include the deltas where the predicate returns a truthy value for this function when passed the object of a triple.
+ * @returns {array(namedNode)} An array of namedNodes representing the filtered results that are always IRIs.
+ */
+export async function getSubjectsWithFunctions(
+  deltas,
+  subjectPredicate,
+  predicatePredicate,
+  objectPredicate
+) {
+  const filtered = deltas
+    .map((changeset) => changeset.inserts)
+    .filter((inserts) => inserts.length > 0)
+    .flat()
+    .filter(subjectPredicate)
+    .filter(predicatePredicate)
+    .filter(objectPredicate)
+    .map((insert) => namedNode(insert.subject.value));
+  return filtered;
+}
