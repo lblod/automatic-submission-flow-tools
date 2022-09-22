bfd280b310ac64456a0adf5b78b6dda53f35eda6
benjay10 <ben.behaeghe@redpencil.io>
2022-09-22 05:12:25 +0200

    Fixed: some function where indicated as async
    This was not the case.

--- deltas.js
+++ deltas.js
@@ -3,8 +3,9 @@
  * @description Filter delta messages from the delta-notifier.
  */
 
+import * as uti from './utils.js';
 import * as N3 from 'n3';
-const { namedNode } = N3.DataFactory;
+const { quad } = N3.DataFactory;
 
 /**
  * Get subjects after filtering the delta messages on a predicate and object.
@@ -24,7 +25,7 @@ export async function getSubjects(deltas, predicate, object) {
     .flat()
     .filter((insert) => insert.predicate.value === predicate.value)
     .filter((insert) => insert.object.value === object.value)
-    .map((insert) => namedNode(insert.subject.value));
+    .map((insert) => uti.parseSparqlJsonTerm(insert.subject));
   return filtered;
 }
 
@@ -53,6 +54,34 @@ export async function getSubjectsWithFunctions(
     .filter(subjectPredicate)
     .filter(predicatePredicate)
     .filter(objectPredicate)
-    .map((insert) => namedNode(insert.subject.value));
+    .map((insert) => uti.parseSparqlJsonTerm(insert.subject));
+  return filtered;
+}
+
+/**
+ * Get triples after filtering the delta messages with the help of subject, predicate and object 'predicate' functions.
+ *
+ * @public
+ * @function
+ * @param {array(object)} deltas - The delta messages used to filter through. The datastructure is the typical delta message format with inserts, deletes, and arrays with objects with s-p-o properties.
+ * @param {namedNode} subjectPredicate - Include the deltas where the predicate returns a truthy value for this function when passed the subject of a triple.
+ * @param {namedNode} predicatePredicate - Include the deltas where the predicate returns a truthy value for this function when passed the predicate of a triple.
+ * @param {namedNode} objectPredicate - Include the deltas where the predicate returns a truthy value for this function when passed the object of a triple.
+ * @returns {array(quad)} An array of quads representing the filtered data as triples.
+ */
+export function getTriplesWithFunctions(
+  deltas,
+  subjectPredicate,
+  predicatePredicate,
+  objectPredicate
+) {
+  const filtered = deltas
+    .map((changeset) => changeset.inserts)
+    .filter((inserts) => inserts.length > 0)
+    .flat()
+    .filter(subjectPredicate)
+    .filter(predicatePredicate)
+    .filter(objectPredicate)
+    .map(uti.parseSparqlJsonBindingQuad);
   return filtered;
 }
