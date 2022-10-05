bcf0fd785fa48c54ee57ce62f1135cb0cee6ce3d
benjay10 <ben.behaeghe@redpencil.io>
2022-10-05 22:18:40 +0200

    Added more getters for Submission, changes to docs

--- asfSubmissions.js
+++ asfSubmissions.js
@@ -20,14 +20,44 @@ const { literal } = N3.DataFactory;
  * @public
  * @async
  * @function
+ * @param {namedNode} submission - Represents the IRI of the Submission you want more information about.
+ * @returns {object} A JavaScript object with the following information about the Submission: `{namedNode} submission`, `{literal} documentUrl`, `{namedNode} submittedDocument`, `{namedNode} status`, `{namedNode} remoteDataObject`, `{namedNode} physicalFile` and `{namedNode} graph` that contains information about the Submission.
+ */
+export async function getSubmissionInfo(submission) {
+  const infoQuery = `
+    ${cts.SPARQL_PREFIXES}
+    SELECT ?submission ?status ?documentUrl ?remoteDataObject ?physicalFile ?submittedDocument ?graph WHERE {
+      GRAPH ?graph {
+        ?physicalFile
+          nie:dataSource ?remoteDataObject .
+        ?submission
+          nie:hasPart ?remoteDataObject ;
+          prov:atLocation ?documentUrl ;
+          dct:subject ?submittedDocument .
+        BIND ( ${rst.termToString(submission)} as ?submission )
+      }
+    } LIMIT 1`;
+  const response = await mas.querySudo(infoQuery);
+  const sparqlJsonParser = new sjp.SparqlJsonParser();
+  const parsedResults = sparqlJsonParser.parseJsonResults(response);
+  return parsedResults[0];
+}
+
+/**
+ * Get some information about the Submission, its document and its downloaded file.
+ *
+ * @public
+ * @async
+ * @function
  * @param {namedNode} remoteDataObject - Represents the IRI of the remote data object that is linked to a submission.
- * @returns {array(object)} An array of objects with the structure: `{ submission: namedNode, documentUrl: literal, file: namedNode, submittedDocument: namedNode }` that contains information about the Submission.
+ * @returns {array(object)} An array of objects with the structure: `{ submission: namedNode, documentUrl: literal, file: namedNode, submittedDocument: namedNode, status: namedNode, graph: namedNode }` that contain information about the Submissions.
  */
 export async function getSubmissionInfoFromRemoteDataObject(remoteDataObject) {
   const remoteDataObjectSparql = rst.termToString(remoteDataObject);
   const infoQuery = `
     ${cts.SPARQL_PREFIXES}
-    SELECT ?submission ?documentUrl ?file ?submittedDocument ?graph WHERE {
+    SELECT ?submission ?documentUrl ?file ?submittedDocument ?status ?graph
+    WHERE {
       GRAPH ?graph {
         ?file
           nie:dataSource ${remoteDataObjectSparql} .
@@ -37,14 +67,74 @@ export async function getSubmissionInfoFromRemoteDataObject(remoteDataObject) {
           dct:subject ?submittedDocument .
       }
     }`;
-
   const response = await mas.querySudo(infoQuery);
   const sparqlJsonParser = new sjp.SparqlJsonParser();
   return sparqlJsonParser.parseJsonResults(response);
 }
 
+/**
+ * Get some information about the Submission, its document and its downloaded file.
+ *
+ * @public
+ * @async
+ * @function
+ * @param {namedNode} task - The task the submission is related to.
+ * @returns {object} A JavaScript object with the following information about the Submission: `{namedNode} submission`, `{literal} documentUrl`, `{namedNode} submittedDocument`, `{namedNode} status` and `{namedNode} graph` that contains information about the Submission.
+ */
+export async function getSubmissionInfoFromTask(task) {
+  const infoQuery = await mas.querySudo(`
+    ${cts.SPARQL_PREFIXES}
+    SELECT ?submission ?documentUrl ?submittedDocument ?status ?graph
+    WHERE {
+      GRAPH ?graph {
+        ${rst.termToString(task)}
+          a task:Task ;
+          dct:isPartOf ?job .
+        ?job prov:generatedBy ?submission .
+        ?submission
+          dct:subject ?submissionDocument ;
+          prov:atLocation ?documentUrl ;
+          adms:status ?status .
+      }
+    } LIMIT 1
+  `);
+  const response = await mas.querySudo(infoQuery);
+  const sparqlJsonParser = new sjp.SparqlJsonParser();
+  const parsedResults = sparqlJsonParser.parseJsonResults(response);
+  return parsedResults[0];
+}
+
+/**
+ * Get some information about the Submission, its document and its downloaded file.
+ *
+ * @public
+ * @async
+ * @function
+ * @param {literal} id - Representing the id of the Submitted Document that is related to the Submission.
+ * @returns {object} A JavaScript object with the following information about the Submission: `{namedNode} submission`, `{literal} documentUrl`, `{namedNode} submittedDocument`, `{namedNode} status` and `{namedNode} graph` that contains information about the Submission.
+ */
+export async function getSubmissionInfoFromSubmissionDocumentId(id) {
+  const infoQuery = await mas.querySudo(`
+    ${cts.SPARQL_PREFIXES}
+    SELECT ?submission ?documentUrl ?submittedDocument ?status ?graph
+    WHERE {
+      GRAPH ?graph {
+        ?submittedDocument
+          mu:uuid ${rst.termToString(id)} .
+        ?submission
+          dct:subject ?submissionDocument ;
+          adms:status ?status .
+      }
+    } LIMIT 1
+  `);
+  const response = await mas.querySudo(infoQuery);
+  const sparqlJsonParser = new sjp.SparqlJsonParser();
+  const parsedResults = sparqlJsonParser.parseJsonResults(response);
+  return parsedResults[0];
+}
+
 ////////////////////////////////////////////////////////////////////////////////
-// Submissions Documents
+// Submission Documents
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
